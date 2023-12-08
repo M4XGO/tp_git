2aa55027f505d529ee21e673d99e3dbf0f088e48
totofile <theophilenp@gmail.com>
2023-12-08 10:42:20 +0100

    feat(js):modif-element

---
+++ modify.js
@@ -0,0 +1,13 @@
+// Description: Modify an element's text content.
+// Your code here
+//examplple ...
+console.log('modify.js');
+
+// Get the element you want to modify
+const element = document.getElementById('myElement');
+
+// Modify the element's text content
+element.textContent = 'New text content';
+
+// Log the modified element
+console.log(element);
